d5fa94c4e0b23a781ed47e6634470a99510c34a1
plinygg <aidan.chang@outlook.com>
2024-04-19 21:41:48 -0500

    removed faux file

--- wordlesolver.py
+++
@@ -1,8 +0,0 @@
-w = open('fives.txt', 'r')
-
-def removewords(word):
-    words = w.readlines()
-    while len(words) > 1:
-        input('COMMANDS:\nGray(type "gray" followed by the letters)\nYellow(type "yellow" followed by the letter and where the letter is)\nGreen(type "green" followed by the letter and where the letter is)\n')
-        
-removewords(1)
