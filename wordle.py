b8ae43c04413771427883d9e30e15a84a3ccea33
plinygg <aidan.chang@outlook.com>
2024-04-22 21:39:32 -0500

    solved

--- wordle.py
+++ wordle.py
@@ -1,3 +1,5 @@
+import os
+os.chdir("D:\\vsprojects\\wordle solver\\wordle-1")
 w = open('fives.txt', 'r')
 
 words = []
@@ -35,29 +37,57 @@ while len(words) > 0:
     elif first_input == "yellow": # yellow f 5, g 3
         letter_and_value = inputs.lower().split(',')
         checker = inputs.lower().split(' ')
-        print(letter_and_value)
         if len(checker) == 1:
             print('Please input a letter followed by where the yellow letter is. If there are multiple, please use a comma.')
-        else:
+        else: 
             letter_and_value = inputs.lower()[6:].split(',')
             temp = []
             for pair in letter_and_value:
                 pair = pair.strip()
                 temp.append(pair)
             letter_and_value = temp
-            print(letter_and_value)
+            # print(letter_and_value)
+        yellow_list = []
+        for word in words:
+            if all(word[int(pair[-1])-1] != pair[0] for pair in letter_and_value) and all(pair[0] in word for pair in letter_and_value):
+                    yellow_list.append(word)
+        words = yellow_list
+        letters = ""
+        places = ""
         for pair in letter_and_value:
-            if not pair[0].isnumeric(): # if the first value in every pair is not a number:
-                pass
+            letters = letters + ' ' + str(pair[0])
+            places = places + ' ' + str(pair[-1])
+        print('Words with letter(s){a} at place(s){b} (respectively) have been removed.'.format(a=letters, b=places))
+    elif first_input == "green":
+        letters_and_value = inputs.lower().split(',')
+        checker1 = inputs.lower().split(' ')
+        if len(checker1) == 1:
+            print('Please input a letter followed by where the green letter is. If there are multiple, please use a comma.')
+        else: 
+            letters_and_value = inputs.lower()[6:].split(',')
+            temp2 = []
+            for pair in letters_and_value:
+                pair = pair.strip()
+                temp2.append(pair)
+            letters_and_value = temp2
+        green_list = []
+        for word in words:
+            if not pair[0].isnumeric():
+                if all(word[int(pair[-1])-1] == pair[0] for pair in letters_and_value):
+                    green_list.append(word)
             else:
                 print('Improper use of command.')
-    elif first_input == "green":
-        pass
+        words = green_list
+        letters2 = ""
+        places2 = ""
+        for pair in letters_and_value:
+            letters2 = letters2 + ' ' + str(pair[0])
+            places2 = places2 + ' ' + str(pair[-1])
+        print('Words with letter(s){a} at place(s){b} (respectively) have been included.'.format(a=letters2, b=places2))
     elif first_input == "quit":
         print('\nProgram ended.')
         break
     elif first_input == "skibidi":
-        print('\n\nskibidi fortnite\n-Isabella Alban\n')
+        print('\nskibidi fortnite\n-Isabella Alban\n\n')
     else:
         print('{a} is not a command. Choose a command:'.format(a=inputs))
-
